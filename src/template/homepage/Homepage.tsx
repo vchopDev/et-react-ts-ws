a402c342f833645cb70caca772c4ae63f5d36373
vchopDev <victor.martins@evoluzionetelematica.it>
2019-06-21 13:37:40 +0200

    step 6 - adding homepage

---
+++ src/template/homepage/Homepage.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import Card from './../../components/card/Card';
+
+
+class Catalog extends React.Component {
+  public render() {
+    return (
+      <div className="catalog">
+        <h1>HOMEPAGE</h1>
+          {/* <Card /> */}
+        </div>
+    );
+  }
+}
+
+export default Catalog;
